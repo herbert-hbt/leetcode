f763b2db23ec0fcdd20f7a670a201bc1364c1f60
Herbert <admin@admindeMacBook-Pro.local>
2020-10-29 13:34:18 +0800

    76

---
+++ 76.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    const minStr = "";
+    const sArr = s.split("");
+    const tArr = t.split("");
+    for(let i=0;i<tArr.length;i++){
+        if(sArr.){
+
+        }
+    }
+};
+
+minWindow("ADOBECODEBANC","ABC")
